3b29e6ec60760bc489758148931d53e26275d7d3
biqqles <biqqles@pm.me>
2023-01-22 21:41:32 +0000

    hindent

--- src/Dependencies.hs
+++ src/Dependencies.hs
@@ -1,8 +1,11 @@
-module Dependencies (collect_revdeps) where
+module Dependencies
+  ( collect_revdeps
+  ) where
 
 import Data.List.Split (splitOn)
 import System.Process (readProcess)
 import Text.Regex.PCRE ((=~))
+
 import Ergonomics
 
 eopkg_info :: [String] -> IO [String]
@@ -13,21 +16,22 @@ eopkg_info pkgs = do
 revdeps_of :: [String] -> IO [String]
 revdeps_of pkgs = do
   infos <- eopkg_info pkgs
-  return $ infos
-            |> filter is_remote
-            |> map extract_revdeps
-            |> concat
-            |> filter (not.is_subpackage)
+  return $
+    infos
+    |> filter is_remote
+    |> map extract_revdeps
+    |> concat
+    |> filter (not . is_subpackage)
   where
     is_remote = (`contains` "Package found")
-    is_subpackage pkg = any (pkg `ends_with`) ["dbginfo", "devel"]
+    is_subpackage pkg = (pkg `ends_with`) `any` ["dbginfo", "devel"]
     extract_revdeps = words . (=~ "[^:]+\\z")
 
 collect_revdeps :: [String] -> IO [String]
 collect_revdeps start = do
   revdeps <- revdeps_of start
-  if null revdeps then
-    return start
-  else do
-    tree <- collect_revdeps revdeps
-    return (start ++ revdeps ++ tree)
+  if null revdeps
+    then return start
+    else do
+      tree <- collect_revdeps revdeps
+      return (start ++ revdeps ++ tree)
--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,25 @@
 import System.Environment (getArgs)
-import Ergonomics (unique)
-import Dependencies
 
-data Action = Command ([String] -> IO ()) [String] | Error String
+import Dependencies
+import Ergonomics (unique)
+
+data Action
+  = Command ([String] -> IO ()) [String]
+  | Error String
 
 depends_on :: [String] -> IO ()
-depends_on pkgs = collect_revdeps pkgs
-                    >>= putStr.unlines.unique
+depends_on pkgs = collect_revdeps pkgs >>= putStr . unlines . unique
 
 parse_command :: [String] -> Action
 parse_command [] = Error "no command given"
 parse_command [c] = Error $ "missing argument(s) for command: " <> c
-parse_command (c:args) = case c of
-                           "depends-on" -> Command depends_on args
-                           otherwise -> Error $ "unknown command: " <> c
+parse_command (c:args) =
+  case c of
+    "depends-on" -> Command depends_on args
+    otherwise -> Error $ "unknown command: " <> c
 
 main = do
-    args <- getArgs
-    case (parse_command args) of
-      Command f args -> f args
-      Error msg -> putStrLn $ "Error - " <> msg
+  args <- getArgs
+  case (parse_command args) of
+    Command f args -> f args
+    Error msg -> putStrLn $ "Error - " <> msg
